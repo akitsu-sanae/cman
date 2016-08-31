856a8c333a1be902caf8d50d8999a0340a94912f
akitsu-sanae <akitsu.sanae@gmail.com>
2016-09-01 04:08:45 +0900

    new: make directory depending on cman.toml

--- src/new.rs
+++ src/new.rs
@@ -8,6 +8,7 @@
 
 use std::fs;
 use std::env;
+use config::Config;
 
 fn mkdir(path: &String) {
     fs::create_dir_all(path.as_str())
@@ -15,18 +16,27 @@ fn mkdir(path: &String) {
 }
 
 pub fn command(args: Vec<String>) {
-    let ref path = args[1];
-    mkdir(path);
-    mkdir(&format!("{}/build/obj", path));
-    mkdir(&format!("{}/include", path));
-    mkdir(&format!("{}/src", path));
-
+    let ref pack_name = args[1];
     let cman_dir = env::var("CMAN_CONFIG_PATH")
         .expect("CMAN_CONFIG_PATH is not set");
 
-    fs::copy(format!("{}/main.cpp", cman_dir), format!("{}/src/main.cpp", path))
+    let config = Config::load(format!("{}/cman.toml", cman_dir).as_str())
+        .expect("missing default cman.toml");
+
+    mkdir(pack_name);
+    mkdir(&format!("{}/build/obj", pack_name));
+    for inc in config.path.includes {
+        if inc.as_bytes()[0] != '.' as u8 { continue } // maybe, this is not relative path
+        mkdir(&format!("{}/{}", pack_name, inc));
+    }
+
+    if config.path.src_dir.as_bytes()[0] == '.' as u8 {
+        mkdir(&format!("{}/{}", pack_name, config.path.src_dir));
+    }
+
+    fs::copy(format!("{}/main.cpp", cman_dir), format!("{}/{}/main.cpp", pack_name, config.path.src_dir))
         .expect("can not copy main.cpp");
-    fs::copy(format!("{}/cman.toml", cman_dir), format!("{}/cman.toml", path))
+    fs::copy(format!("{}/cman.toml", cman_dir), format!("{}/cman.toml", pack_name))
         .expect("can not copy cman.toml");
 }
 
